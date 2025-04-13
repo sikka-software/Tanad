0842a1c42fbef5e10ee9f59a01cb8def188aaf7b
MansourKira <mansour.benmansour@esprit.tn>
2025-04-13 12:17:35 +0100

    chore: init billing page

--- src/pages/billing.tsx
+++ src/pages/billing.tsx
@@ -2,15 +2,23 @@
 import { GetStaticProps } from "next";
 import { useTranslations } from "next-intl";
 
+import { useAuth } from "@/components/UserContext";
 import CustomPageMeta from "@/components/landing/CustomPageMeta";
 
 export default function Billing() {
   const t = useTranslations();
+  const { user } = useAuth();
   return (
     <>
       <CustomPageMeta title={t("Billing.title")} description={t("Billing.description")} />
       <main className={`flex flex-col items-center justify-between`}>
-        {t("General.coming_soon")}
+        {user?.subscribed_to ? (
+          <div>
+            <h1>{t("Billing.title")}</h1>
+          </div>
+        ) : (
+          <div>{t("Billing.upgrade_dialog.title")}</div>
+        )}
       </main>
     </>
   );
