7c3c6c8e9e9b0342eae798f7baab5d8f2e69a78c
Zakher Masri <zakhermasri@gmail.com>
2025-04-15 15:00:54 +0300

    refactor: Add admin-specific development and build scripts to package.json for job and admin portals, enhancing workflow flexibility and environment management.

---
+++ .github/pull_request_template.md
@@ -0,0 +1,12 @@
+**Fix issue** [link to issue]
+
+**Changes proposed in this Pull Request**
+
+- First change
+- Second change
+
+**Smoke tests**
+_Describe the testing process_
+
+- [ ] Test 1
+- [ ] Test 2
---
+++ apps/admin-portal/.nixpacks/build.sh
@@ -0,0 +1,1 @@
+docker build . -f ./.nixpacks/Dockerfile -t 3f2a9b9b-7c2f-4d49-a9c9-12c5a961bba4 --build-arg CI=true --build-arg NIXPACKS_METADATA=node --build-arg NODE_ENV=production --build-arg NPM_CONFIG_PRODUCTION=false
